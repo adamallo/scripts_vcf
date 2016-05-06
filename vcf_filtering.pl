ef046db5ff948a7d09b03b1467fd3e76ebdb8c71
Diego Mallo <dmalload@asu.edu>
2016-05-06 16:10:11 -0700

    Added new parameters to vcf_filtering.pl

--- vcf_filtering.pl
+++ vcf_filtering.pl
@@ -4,14 +4,14 @@ use warnings;
 
 use Getopt::Long qw(GetOptions);
 Getopt::Long::Configure qw(gnu_getopt);
+use File::Copy;
 
 my $SNPSIFT_DIR=$ENV{'SNPSIFT_DIR'};
-my ($qual,$AtoC,$AtoG,$AtoT,$CtoA,$CtoG,$CtoT,$GtoA,$GtoC,$GtoT,$TtoA,$TtoC,$TtoG,$min_cover,$min_reads_strand,$min_reads_alternate,$isvar); #Only some options to start with
-$isvar="";
+my ($qual,$AtoC,$AtoG,$AtoT,$CtoA,$CtoG,$CtoT,$GtoA,$GtoC,$GtoT,$TtoA,$TtoC,$TtoG,$min_cover,$min_reads_strand,$min_reads_alternate,$freq_alt,$isvar);
 my $input_file="";
 my $output_file="";
 my $help;
-my $usage="Usage: $0 -i input_file -o output_file [options]\n\n\nOptions:\n--------\n\t-q/--qual : quality filter\n\t--atoc : filter out mutations from A to C\n\t--atog :  filter out mutations from A to G\n\t--atot : filter out mutations from A to T\n\t--ctoa :  filter out mutations from C to A\n\t--ctog : filter out mutations from C to G\n\t--ctot :  filter out mutations from C to T\n\t--gtoa : filter out mutations from G to A\n\t--gtoc :  filter out mutations from G to C\n\t--gtot : filter out mutations from G to T\n\t--ttoa :  filter out mutations from T to A\n\t--ttoc : filter out mutations from T to C\n\t--ttog : filter out mutations from T to G\n\t-m/--min_coverage :minimum coverage per locus\n\t-s/--min_reads_strand : minimum number of reads per strand\n\t-a/--min_reads_alternate : minimum number of reads for the alternative allele\n\t--isvar : filter out variants with genotype ref/ref in the given sample (starting in 1)\n\t\n\n";
+my $usage="Usage: $0 -i input_file -o output_file [options]\n\n\nOptions:\n--------\n\t-q/--qual : quality filter\n\t--atoc : filter out mutations from A to C\n\t--atog :  filter out mutations from A to G\n\t--atot : filter out mutations from A to T\n\t--ctoa :  filter out mutations from C to A\n\t--ctog : filter out mutations from C to G\n\t--ctot :  filter out mutations from C to T\n\t--gtoa : filter out mutations from G to A\n\t--gtoc :  filter out mutations from G to C\n\t--gtot : filter out mutations from G to T\n\t--ttoa :  filter out mutations from T to A\n\t--ttoc : filter out mutations from T to C\n\t--ttog : filter out mutations from T to G\n\t-m/--min_coverage :minimum coverage per locus\n\t-s/--min_reads_strand : minimum number of reads per strand\n\t-a/--min_reads_alternate : minimum number of reads for the alternative allele\n\t--freq_alt : frequency of reads supporting the alternative allele\n\t--isvar : filter out variants with genotype ref/ref in the given sample (starting in 1)\n\t\n\n";
 my $exe;
 
 ########### SNPSIFT Util detection #########################
@@ -56,7 +56,8 @@ else
 	'ttog=i' => \$TtoG,
 	'min_coverage|m=i' => \$min_cover, 
 	'min_reads_strand|s=i' => \$min_reads_strand,
-	'min_reads_alternate|a=i' => \$min_reads_alternate,
+	'min_reads_alternate|a=s' => \$min_reads_alternate,
+	'freq_alt=s' => \$freq_alt,
     'isvar=s' => \$isvar,
 	'help|h' => \$help,
 		)) or ((! -f $input_file) || ($output_file eq "") || $help)) and die $usage;
@@ -165,11 +166,25 @@ if ($min_cover)
 {
 	if ($platypus)
 	{
-		$variable="TC";
+		my @temp=split("_",$min_cover);
+		if(scalar @temp ==1) #No genotype information. By default, total coverage.
+		{
+			$variable="TC";
+
+		}
+		elsif (scalar @temp ==2) #Genotype information. Applyied by genotype
+		{
+			$variable="GEN[$temp[0]].NR[*]";
+			$min_cover=$temp[1];
+		}
+		else
+		{
+			die "--min_coverage does not have the proper format. It must by just the value, or a genotype indicator, _ , and the value. Example: 1_10\n";
+		}
 	}
 	elsif ($multisnv)
 	{
-		$variable="GEN[ALL].DP"
+		$variable="GEN[ALL].DP[*]"
 	}
 	else 
 	{
@@ -197,7 +212,19 @@ if ($min_reads_alternate)
 {
 	if ($platypus)
 	{
-		$out_filter.=" ( GEN[*].NV > $min_reads_alternate ) &";
+		my @temp=split("_",$min_reads_alternate);
+		if(scalar @temp == 1) #There is no genotype information. By default, if any fulfill the filtering, they are kept.
+		{
+			$out_filter.=" ( GEN[*].NV[*] >= $min_reads_alternate ) &";
+		}
+		elsif (scalar @temp == 2)
+		{
+			$out_filter.=" ( GEN[$temp[0]].NV[*] >= $temp[1] ) &";
+		}
+		else
+		{
+			die "--min_reads_alternate does not have the proper format. It must by just the value, or a genotype indicator, _ , and the value. Example: 1_10\n";
+		}
 	}
 	elsif($multisnv)
 	{
@@ -208,9 +235,9 @@ if ($min_reads_alternate)
 		die "This variant caller is not supported by this script\n";
 	}
 }
-if ($isvar ne "")
+if ($isvar)
 {
-    $out_filter.= " ( isVariant( GEN[$isvar] ) ) &";
+	$out_filter.= " ( isVariant( GEN[$isvar] ) ) &";
 }
 
 $out_filter eq "" and die "ERROR: No filtering options have been specified\n\n$usage"; 
@@ -224,6 +251,115 @@ chop($out_filter);
 
 substr($out_filter,0,1)=""; ##Removing extra space in the begining
 system("cat $input_file | java -jar $exe filter \"$out_filter\" > $output_file");
-exit;
 
 #########################################################
+
+##All this looks like a really bad strategy.
+#Alternative strategy (some thoughts)
+#Design an array of keys from the columns + Genotypes and Format
+#Generate a hash of variants, with value an array of values following the order of the keysarray
+#This way, I could generate code from the restrictions, and then apply it to the hash+array thing. Using eval to execute strings.
+#There will have to be a loop, since some filters generate multiple instructions (Gene[*] and Gene[ALL]).
+
+if ($freq_alt) ##So far only this option requires my own filtering step. This strategy will have to be slightly changed if we implement more.
+{
+	if ($platypus)
+	{
+		
+		my @temp=split("_",$freq_alt);
+		my $comment;
+		if(scalar @temp == 1) #There is no genotype information. By default, if any fulfill the filtering, they are kept.
+		{
+			die "The general option has not been yet implemented for the variable --freq_alt";
+		}
+		elsif (scalar @temp == 2)
+		{
+			$comment="##Filtered with vcf_filt. Filter GEN[$temp[0]].NV/GEN[$temp[0]].NR >= $temp[1]";
+		}
+		else
+		{
+			die "--min_reads_alternate does not have the proper format. It must by just the value, or a genotype indicator, _ , and the value. Example: 1_10\n";
+		}
+
+		#### DIY Filtering options
+		#########################################################
+		
+		##Copy the $output_file to $output_file_snipeff
+	
+		move($output_file,"${output_file}_SnipEff");
+		open(my $IFILE, "${output_file}_SnipEff");
+		my @content=<$IFILE>;
+		close $IFILE;
+		open(my $OFILE, ">$output_file");
+		my $flag=0;
+		my @line;
+		my @gen;
+		my $format;
+		my @genids;
+		my $NV;
+		my $NR;
+		my @NVvalues;
+		my @NRvalues;
+
+		for (my $i=0; $i<scalar @content; ++$i)
+		{
+			if ($flag==0 and $content[$i]=~/^##/)
+		        {
+		            print($OFILE $content[$i]);
+		        }
+		        elsif($flag==0)
+		        {
+		            print($OFILE "$comment\n$content[$i]");
+		            $flag=1;
+		        }
+		        else
+		        {
+				@line=split("\t",$content[$i]);
+				@gen=split(":",$line[9+$temp[0]]);
+				#print "DEBUG: @gen\n"; 
+				$format=$line[8];
+				@genids=split(":",$format);
+				for (my $j=0; $j<scalar @genids; ++$j)
+				{
+					if($genids[$j] eq "NV")
+					{
+						$NV=$j;
+					}
+					elsif($genids[$j] eq "NR")
+					{
+						$NR=$j;
+					}		
+				}
+				#print "DEBUG: NV=$NV value=$gen[$NV] NR=$NR value=$gen[$NR]\n";
+				@NRvalues=split(",",$gen[$NR]);
+				@NVvalues=split(",",$gen[$NV]);
+				for (my $j=0; $j<scalar @NRvalues; ++$j)
+				{
+					if ($NRvalues[$j] != 0)
+					{
+						if ($NVvalues[$j]/($NRvalues[$j]+0.0) >= $temp[1])
+						{
+							print($OFILE $content[$i]);
+							last;
+						}
+					}
+					elsif ($temp[1] <= 0) ##If the filter is 0, we don't care if we can't calculate it.
+					{
+						print($OFILE $content[$i]);
+						last;
+					}
+				}
+		        }
+		}
+		close $OFILE;
+	}
+	elsif($multisnv)
+	{
+		die "This option is not compatible with multisnv\n";
+	}
+	else 
+	{
+		die "This variant caller is not supported by this script\n";
+	}
+}
+exit;
