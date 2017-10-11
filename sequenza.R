ccf7251fb2a6d9683575488e8b5a5c73f4faa4f0
Diego Mallo <dmalload@asu.edu>
2017-10-11 01:47:05 -0700

    Big advance in cnvcallancomp, using ascat to perform the segmentation of A and B together

--- sequenza.R
+++ sequenza.R
@@ -9,21 +9,23 @@ id=basename(id)
 #dir.create(outdir)
 ncores=as.numeric(options[3])
 
+
+#I don't really need this
 mydataplotgc=read.seqz(name) ##This takes like 3 min
 gcstats=gc.norm(x = mydataplotgc$depth.ratio, gc = mydataplotgc$GC.percent)
 gc.vect <- setNames(gcstats$raw.mean, gcstats$gc.values)
 mydataplotgc$adjusted.ratio <- mydataplotgc$depth.ratio / gc.vect[as.character(mydataplotgc$GC.percent)]
-
-
 pdf(paste0(outdir,"/GCnormalization.pdf"))
 par(mfrow = c(1,2), cex = 1, las = 1, bty = 'l')
 matplot(gcstats$gc.values, gcstats$raw, type = 'b', col = 1, pch = c(1, 19, 1), lty = c(2, 1, 2), xlab = 'GC content (%)', ylab = 'Uncorrected depth ratio')
 legend('topright', legend = colnames(gcstats$raw), pch = c(1, 19, 1))
 hist2(mydataplotgc$depth.ratio, mydataplotgc$adjusted.ratio, breaks = prettyLog, key = vkey, panel.first = abline(0, 1, lty = 2), xlab = 'Uncorrected depth ratio', ylab = 'GC-adjusted depth ratio')
 dev.off()
+###
+
 
 chromosomes=c(seq(1,22),"X")
-mydata=sequenza.extract(file=name,chromosome.list=chromosomes) ##I am excluding the GL "chromosomes" and the mitocondria since they generate an error in sequenza, and the Y, since these samples are all from females
+mydata=sequenza.extract(file=name,chromosome.list=chromosomes,min.reads=20,min.reads.normal=10,min.reads.baf=20,mufreq.treshold=0.25,max.mut.types=3) ##I am excluding the GL "chromosomes" and the mitocondria since they generate an error in sequenza, and the Y, since these samples are all from females
 
 for (chr in 1:length(chromosomes)) {
 print(chromosomes[chr])
@@ -33,5 +35,5 @@ try(chromosome.view(mut.tab = mydata$mutations[[chromosome]], baf.windows = myda
 dev.off()
 }
 
-cp <- sequenza.fit(mydata,mc.cores=ncores,chromosome.list=chromosomes,female=TRUE,XY=c("X","Y"))
-sequenza.results(mydata,cp.table=cp,sample.id=id,out.dir=outdir,chromosome.list=chromosomes,female=TRUE,XY=c("X","Y"))
+cp <- sequenza.fit(mydata,mc.cores=ncores,chromosome.list=chromosomes,female=TRUE)
+sequenza.results(mydata,cp.table=cp,sample.id=id,out.dir=outdir,chromosome.list=chromosomes,female=TRUE)
