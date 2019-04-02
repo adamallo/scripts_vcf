ad7746aea33fca34692e6618e6a3b83a8e96f9a5
Diego Mallo <dmalload@asu.edu>
2019-04-01 18:14:03 -0700

    Improvements in get_variants_multiple, FASTA output for phylogenetic reconstruction among others

---
+++ raxml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name=$1
+
+raxmlHPC-AVX2 -f d -m ASC_GTRGAMMA -n ${name}_ml -s $name --asc-corr=lewis -p 2020 -N 20
+raxmlHPC-AVX2 -f d -m ASC_GTRGAMMA -n ${name}_bs -s $name --asc-corr=lewis -p 2020 -b 2020 -N 100
+raxmlHPC-AVX2 -f b -m ASC_GTRGAMMA -n ${name}_bs_tree -z RAxML_bootstrap.${name}_bs -t RAxML_bestTree.${name}_ml
