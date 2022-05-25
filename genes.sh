47809b26e48973740dbc353bb6eab0318caa8d47
Diego Mallo <dmalload@asu.edu>
2022-05-25 09:06:06 -0700

    Adding germline information to the analysis of commonly mutated breast cancer genes

--- genes.sh
+++ genes.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-module load bedtools2/2.24.0
 module load bcftools/1.12.0
 
 mainDir="/scratch/dmalload/dcis/aim4Genes/data"
@@ -51,6 +50,24 @@ function getCountsUnprocessed {
     echo $(echo -e "$(echo "$(bcftools view -i 'FILTER="PASS" || FILTER="alleleBias"' ${vcfFiles[0]} 2>/dev/null)""\n""$(bcftools view -i 'FILTER="PASS" || FILTER="alleleBias"' ${vcfFiles[1]} 2>/dev/null)" | perl -lane 'BEGIN{$,="\t"}{/^#/ or print($F[0],$F[1]-1,$F[1])}' | sort -k1,1 -k2,2n | bedtools merge -i stdin | bedtools intersect -a $bedFile -b stdin -wb | perl -lane '{print $F[3]}')""\n""$(perl -lane '{print $F[3]}' $bedFile)" | sort | uniq -c | perl -lane '$F[1] =~ /^\s*$/ or {print($F[0]-1)}')
 }
 
+# Uses a vcf file and a bedfile, and returns a string of space-separated values with the number of vcf elements in each of the interval (genes) in the bed file
+function getCountsUnprocessedSingleVCF {
+    bedFile=$1
+    vcfFile=$2
+
+    #To count the variants for each gene keeping all of them, I am concatenating the output of the bedtools intersect with the list of genes, sorting and counting the number of each of them, and then substracting one
+    echo $(echo -e "$(perl -lane 'BEGIN{$,="\t"}{/^#/ or print($F[0],$F[1]-1,$F[1])}' $vcfFile | sort -k1,1 -k2,2n | bedtools intersect -a $bedFile -b stdin -wb | perl -lane '{print $F[3]}')""\n""$(perl -lane '{print $F[3]}' $bedFile)" | sort | uniq -c | perl -lane '$F[1] =~ /^\s*$/ or {print($F[0]-1)}')
+}
+
+# Uses a vcf file and a bedfile, and returns a string of space-separated values with the number of vcf elements in each of the interval (genes) in the bed file
+function getCountsFilteredSingleVCF {
+    bedFile=$1
+    vcfFile=$2
+
+    #To count the variants for each gene keeping all of them, I am concatenating the output of the bedtools intersect with the list of genes, sorting and counting the number of each of them, and then substracting one
+    echo $(echo -e "$(bcftools view -i 'FILTER="PASS" || FILTER="alleleBias"' $vcfFile 2>/dev/null | perl -lane 'BEGIN{$,="\t"}{/^#/ or print($F[0],$F[1]-1,$F[1])}' | sort -k1,1 -k2,2n | bedtools intersect -a $bedFile -b stdin -wb | perl -lane '{print $F[3]}')""\n""$(perl -lane '{print $F[3]}' $bedFile)" | sort | uniq -c | perl -lane '$F[1] =~ /^\s*$/ or {print($F[0]-1)}')
+}
+
 # Uses a string of 2 space-separated vcf files a bedfile, and a VCF file with variants to discard, and returns a string of space-separated values with the number of vcf elements in each of the interval (genes) in the bed file
 # In this case, the VCF files (not the ones to discard) need to be filtered for PASS || alleleBias and may contain overlapping entries
 function getCountsUnprocessedDiscardVars {
@@ -77,6 +94,47 @@ function printFun {
     echo -e "$patientString\n$filterString\n$sortedListOfGenes\n$resultString" | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{ii=1;for(i in a){jj=1;for(j in a[ii]){printf"%s"(jj==NR?"\n":FS),a[ii][jj];jj++};ii++}}' FS=" "
 }
 
+# MAIN
+
+usageMessage="\nUsage: $0 [-h] [-b bedfile] [-d dir].\n\nDefaults:\n\tbedfile: $bedFile\n\tdir: $mainDir\n"
+function usage {
+    if [[ $# -eq 1 ]]
+    then
+        echo -e "\nERROR: $1\n""$usageMessage" 1>&2
+    else
+        echo -e "$usageMessage" 1>&2
+    fi
+    
+    exit 1
+}
+
+#The first : indicates silent checking
+#The : after each option indicates that they require an argument
+while getopts ":b:d:h" options
+do
+    case "${options}" in 
+        b)
+            bedFile=${OPTARG}
+            [ -f "$bedFile" ] || usage "bed file not found"
+            ;;
+        d)
+            mainDir=${OPTARG}
+            [ -d "$mainDir" ] || usage "input directory not found"
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage "wrong input option"
+            ;;
+    esac
+done
+
+#After this, there would be positional arguments available
+shift "$((OPTIND-1))"
+[ $# -eq 0 ] || usage "this script does not allow positional arguments"
+
+#Let's top playing with getopts and now this is the real program
 
 # FOREACH FOLDER (PATIENT)
 for patientLog in $mainDir/*.out
@@ -99,23 +157,35 @@ do
 
     if [[ "$vcfFiles" == -1 ]]
     then
-        echo "Skipping the directory/patient $workDir"
+        echo "Skipping the directory/patient ${workDir}. VCF files not found"
         continue
     fi
 
     resultI=$(getCountsUnprocessed $bedFile "$vcfFiles")
     ########
     
-    #Initial without somatic
-    ########################
+    #Germline
+    #########
     vcfN="$workDir/N.vcf"
 
-    if [[ "$vcfFiles" == -1 ]] || [[ ! -f $vcfN ]]
+    if [[ ! -f $vcfN ]]
     then
-        echo "Skipping the directory/patient $workDir"
+        echo "Skipping the directory/patient $workDir, N.vcf file not found"
         continue
     fi
 
+    resultG=$(getCountsUnprocessedSingleVCF $bedFile $vcfN)
+    #########
+    
+    #Filtered germline
+    ##################
+    vcfN="$workDir/N.vcf"
+    resultFG=$(getCountsFilteredSingleVCF $bedFile $vcfN)
+    ##################
+    
+    #Initial without somatic
+    ########################
+    vcfN="$workDir/N.vcf"
     resultIS=$(getCountsUnprocessedDiscardVars $bedFile "$vcfFiles" "$vcfN")
     ########################
     
@@ -199,6 +269,8 @@ do
 
     printFun "$sortedListOfGenes" "$resultI" $patient "I"
     printFun "$sortedListOfGenes" "$resultIS" $patient "IS"
+    printFun "$sortedListOfGenes" "$resultG" $patient "G"
+    printFun "$sortedListOfGenes" "$resultFG" $patient "FG"
     printFun "$sortedListOfGenes" "$resultF" $patient "F"
     printFun "$sortedListOfGenes" "$resultFS" $patient "FS"
     printFun "$sortedListOfGenes" "$resultFSB" $patient "FSB"
